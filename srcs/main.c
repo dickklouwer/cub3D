3a93dbe6ec2b6f1e7bff5fbe388657afef9bfafb
tklouwer <tklouwer@student.codam.nl>
2023-06-22 17:03:34 +0200

    free'd struct

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tklouwer <tklouwer@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/06 11:15:37 by tklouwer      #+#    #+#                 */
-/*   Updated: 2023/06/22 11:41:50 by tklouwer      ########   odam.nl         */
+/*   Updated: 2023/06/22 17:00:53 by tklouwer      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,7 @@ int	main(int argc, char **argv)
 	mlx_loop_hook(game.mlx, ft_hook, &game);
 	mlx_loop(game.mlx);
 	mlx_terminate(game.mlx);
+	free(game.ray);
 	return (EXIT_SUCCESS);
 }
 
